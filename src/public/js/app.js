97b95ffa83a6efefd98a4f293f0286814070508f
degurrrrrr <degurrrrrr@gmail.com>
2022-02-26 20:43:01 +0900

    서버-브라우저 연결 및 buffer 해결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert('hi')
+//socket: 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+    console.log("서버 연결!!!")
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("메세지", message.data, "를 서버로부터 받았다!")
+})
+
+socket.addEventListener("close", () => {
+    console.log("서버로부터 연결 끊어짐!")
+})
+
+setTimeout(() => {
+    socket.send("얍");
+  }, 5000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -11,4 +13,23 @@ app.get("/", (req, res) => res.render("home")); // render
 app.get("/*", (req, res) => res.redirect("/")); //정해두지 않은 url로 접속해도 '/'으로 돌아가게끔!
 
 const handleListen = () => console.log(`Listening on http://localhost:5000`)
-app.listen(5000, handleListen);
+
+const server = http.createServer(app); //http server 생성
+const wss = new WebSocket.Server({ server }) //websocket에서 http 사용 (필수 아님, ws만 작동원하면 이것만)
+
+// 여기 socket은 "연결된 브라우저"를 의미
+wss.on("connection", (socket) => {
+    //connection: 브라우저 연결되었을 때 실행하는거
+    console.log("브라우저 연결!!!") 
+    // 브라우저 꺼졌을 때 
+    socket.on("close", () => console.log("client로부터 연결이 끊어져따"));
+    // 브라우저가 서버에 메세지 보냈을 때
+    socket.on('message', message => {
+        const translatedMessageData = message.toString('utf8');
+        console.log(translatedMessageData);
+      });    
+    // 브라우저에 메세지를 보내도록
+    socket.send("hello!!!!!");
+});
+
+server.listen(5000, handleListen);
