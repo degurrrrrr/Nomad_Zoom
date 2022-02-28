ec2079b2a4211a1873e569e6d72cc28cf41282f4
degurrrrrr <degurrrrrr@gmail.com>
2022-02-28 13:46:18 +0900

    Back-End로 string 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,40 @@
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 //socket: 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+    const msg = { type, payload }; //object로 만들었다!
+    return JSON.stringify(msg); //object를 string으로!
+}
+
 socket.addEventListener('open', () => {
-    console.log("서버 연결!!!")
+    console.log("서버에 연결!")
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("메세지", message.data, "를 서버로부터 받았다!")
+    const li = document.createElement('li');
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("서버로부터 연결 끊어짐!")
 })
+  // "우리에게 event를 준다"
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = ''; //보내고 나서 input 창 비워주는 역할
+}
 
-setTimeout(() => {
-    socket.send("얍");
-  }, 5000);
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value)); // {} : JSon object 객체 전송
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+nicknameForm.addEventListener("submit", handleNicknameSubmit)
--- src/server.js
+++ src/server.js
@@ -17,19 +17,18 @@ const handleListen = () => console.log(`Listening on http://localhost:5000`)
 const server = http.createServer(app); //http server 생성
 const wss = new WebSocket.Server({ server }) //websocket에서 http 사용 (필수 아님, ws만 작동원하면 이것만)
 
+const sockets = [];
+
 // 여기 socket은 "연결된 브라우저"를 의미
+//connection: 브라우저 연결되었을 때 실행하는거
 wss.on("connection", (socket) => {
-    //connection: 브라우저 연결되었을 때 실행하는거
-    console.log("브라우저 연결!!!") 
-    // 브라우저 꺼졌을 때 
+    sockets.push(socket);
+    console.log("서버에서 브라우저와 연결!!!") 
     socket.on("close", () => console.log("client로부터 연결이 끊어져따"));
-    // 브라우저가 서버에 메세지 보냈을 때
-    socket.on('message', message => {
-        const translatedMessageData = message.toString('utf8');
-        console.log(translatedMessageData);
-      });    
-    // 브라우저에 메세지를 보내도록
-    socket.send("hello!!!!!");
+    socket.on("message", message => {
+        const _message = message.toString('utf8');
+        sockets.forEach(aSocket => aSocket.send(_message))
+    });    
 });
 
 server.listen(5000, handleListen);
